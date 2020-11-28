53b40390307818c23cc07fee6e4cf075ff9ecba5
Daniel Boyle <boylede+github@gmail.com>
2020-11-28 14:12:02 -0800

    debug output change

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,14 @@
 use crate::{PartError, PartResult};
 
 pub fn part1(lines: &Vec<String>) -> PartResult {
-    let result = 0;
+    let result = 1;
     // code here
 
     Ok(result.to_string())
 }
 
 pub fn part2(lines: &Vec<String>) -> PartResult {
-    let result = 0;
+    let result = 2;
     // code here
 
     Ok(result.to_string())
