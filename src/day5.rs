63fafb9cb3afa4191d5a92920a02dbb1f17b5e71
Daniel Boyle <boylede+github@gmail.com>
2020-12-04 23:35:45 -0800

    remove unneeded allocations
    each "replace" makes a new String

--- src/day5.rs
+++ src/day5.rs
@@ -1,13 +1,16 @@
 use crate::PartResult;
 
-const ZERO_CHARS: &[char] = &['F', 'L'];
 const ONE_CHARS: &[char] = &['B', 'R'];
 
 pub fn part1(lines: &Vec<String>) -> PartResult {
     let best_seat = lines
         .iter()
-        .map(|seat| seat.replace(ZERO_CHARS, &"0").replace(ONE_CHARS, &"1"))
-        .filter_map(|seat| u32::from_str_radix(&seat, 2).ok())
+        .map(|seat| {
+            seat.chars()
+                .enumerate()
+                .filter(|(_, c)| ONE_CHARS.contains(&c))
+                .fold(0, |a, (i, _)| a + (1 << (9 - i)))
+        })
         .max()
         .unwrap();
 
@@ -17,8 +20,12 @@ pub fn part1(lines: &Vec<String>) -> PartResult {
 pub fn part2(lines: &Vec<String>) -> PartResult {
     let mut seats: Vec<u32> = lines
         .iter()
-        .map(|seat| seat.replace(ZERO_CHARS, &"0").replace(ONE_CHARS, &"1"))
-        .filter_map(|seat| u32::from_str_radix(&seat, 2).ok())
+        .map(|seat| {
+            seat.chars()
+                .enumerate()
+                .filter(|(_, c)| ONE_CHARS.contains(&c))
+                .fold(0, |a, (i, _)| a + (1 << (9 - i)))
+        })
         .collect();
     seats.sort();
     let my_seat: u32 = seats
