07fdaa4b1787633f3ba64d55fd583934460bdfe7
Daniel Boyle <boylede+github@gmail.com>
2020-12-04 22:09:00 -0800

    even more succinct partitioning

--- src/day5.rs
+++ src/day5.rs
@@ -1,19 +1,13 @@
-use crate::{PartError, PartResult};
+use crate::PartResult;
+
+const ZERO_CHARS: &[char] = &['F', 'L'];
+const ONE_CHARS: &[char] = &['B', 'R'];
 
 pub fn part1(lines: &Vec<String>) -> PartResult {
     let best_seat = lines
         .iter()
-        .map(|seat| {
-            let mut chars = seat.chars();
-            let (row, _) = (&mut chars)
-                .take(7)
-                .fold((0, 127), |(min, max), c| cut_range(min, max, c));
-            let (col, _) = (&mut chars)
-                .take(3)
-                .fold((0, 7), |(min, max), c| cut_range(min, max, c));
-            (row, col)
-        })
-        .map(|(row, col)| row * 8 + col)
+        .map(|seat| seat.replace(ZERO_CHARS, &"0").replace(ONE_CHARS, &"1"))
+        .filter_map(|seat| u32::from_str_radix(&seat, 2).ok())
         .max()
         .unwrap();
 
@@ -23,17 +17,8 @@ pub fn part1(lines: &Vec<String>) -> PartResult {
 pub fn part2(lines: &Vec<String>) -> PartResult {
     let mut seats: Vec<u32> = lines
         .iter()
-        .map(|seat| {
-            let mut chars = seat.chars();
-            let (row, _) = (&mut chars)
-                .take(7)
-                .fold((0, 127), |(min, max), c| cut_range(min, max, c));
-            let (col, _) = (&mut chars)
-                .take(3)
-                .fold((0, 7), |(min, max), c| cut_range(min, max, c));
-            (row, col)
-        })
-        .map(|(row, col)| row * 8 + col)
+        .map(|seat| seat.replace(ZERO_CHARS, &"0").replace(ONE_CHARS, &"1"))
+        .filter_map(|seat| u32::from_str_radix(&seat, 2).ok())
         .collect();
     seats.sort();
     let my_seat: u32 = seats[..]
@@ -49,13 +34,4 @@ pub fn part2(lines: &Vec<String>) -> PartResult {
         .next()
         .unwrap();
     Ok(my_seat.to_string())
-}
-
-fn cut_range(min: u32, max: u32, determinant: char) -> (u32, u32) {
-    let mid = ((max - min) / 2) + min;
-    match determinant {
-        'F' | 'L' => (min, mid),
-        'B' | 'R' => (mid + 1, max),
-        _ => panic!("invalid determinant: {}", determinant),
-    }
 }
