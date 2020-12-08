9cfecf93f8e65f3dae46959438c23ec1e7b01f6e
Daniel Boyle <boylede+github@gmail.com>
2020-12-07 18:27:35 -0800

    day 7 rewrite

--- src/day7.rs
+++ src/day7.rs
@@ -3,62 +3,52 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub fn part1(lines: &Vec<String>) -> PartResult {
-    let rules: HashMap<String, Vec<(usize, String)>> = lines
+    let reverse_rules: HashMap<&str, Vec<&str>> = lines
         .iter()
-        .map(|line| {
+        .flat_map(|line| {
             let mut tokens = line.split("bags contain");
-            let bag_type = tokens.next().unwrap().trim().to_string();
-            let children: Vec<(usize, String)> = tokens
+            let bag_type = tokens.next().unwrap().trim();
+            tokens
                 .next()
                 .unwrap()
                 .split(',')
-                .map(|span| {
-                    let mut spans = span.split(' ');
-                    spans.next(); // skip blank
-                    let count = spans.next().unwrap().trim().parse::<usize>().ok();
-                    if let Some(num) = count {
-                        let bag_type: String = spans
-                            .take(2)
-                            .map(|word| {
-                                let mut w = word.to_string();
-                                w.push(' ');
-                                w
-                            })
-                            .collect();
-                        let bg = bag_type.trim().to_string();
-                        (num, bg)
-                    } else {
-                        (0, "".to_string())
-                    }
+                .map(|s| s.trim())
+                .filter_map(|span| {
+                    let mut spans = span.splitn(2, ' ');
+                    spans
+                        .next()
+                        .unwrap()
+                        .trim()
+                        .parse::<usize>()
+                        .ok()
+                        .map(|num| {
+                            (
+                                num,
+                                spans
+                                    .next()
+                                    .unwrap()
+                                    .trim_end_matches('.')
+                                    .trim_end_matches('s')
+                                    .trim_end_matches(&" bag"),
+                            )
+                        })
                 })
-                .collect();
-            // println!("{}: {:?}\n", bag_type, children);
-
-            (bag_type, children)
+                .map(|(_, little)| (little, bag_type))
+                .collect::<Vec<(&str, &str)>>()
         })
-        .collect();
-    let mut reverse: HashMap<String, Vec<String>> = HashMap::new();
-    for (big, littles) in rules.iter() {
-        for (n, little) in littles.iter() {
-            if *n > 0 {
-                reverse
-                    .entry(little.clone())
-                    .or_insert(vec![])
-                    .push(big.to_string());
-            }
-        }
-    }
+        .fold(HashMap::new(), |mut a, (little, big)| {
+            a.entry(little).or_insert(vec![]).push(big);
+            a
+        });
 
-    let mut outer_bags: HashSet<String> = HashSet::new();
-    let mut unvisited_bags: Vec<String> = vec![];
-    unvisited_bags.push("shiny gold".to_string());
+    let mut outer_bags: HashSet<&str> = HashSet::new();
+    let mut unvisited_bags: Vec<&str> = vec![];
+    unvisited_bags.push("shiny gold");
     while unvisited_bags.len() > 0 {
         let next_bag = unvisited_bags.pop().unwrap();
-        // println!("looking for {}", next_bag);
-        if let Some(bag_type) = reverse.get(&next_bag) {
+        if let Some(bag_type) = reverse_rules.get(&next_bag) {
             for bag in bag_type {
                 if !outer_bags.contains(bag) {
-                    // println!("  found {}", bag);
                     unvisited_bags.push(bag.clone());
                     outer_bags.insert(bag.clone());
                 }
@@ -69,78 +59,56 @@ pub fn part1(lines: &Vec<String>) -> PartResult {
 }
 
 pub fn part2(lines: &Vec<String>) -> PartResult {
-    let rules: HashMap<String, Vec<(usize, String)>> = lines
+    let rules: HashMap<&str, Vec<(usize, &str)>> = lines
         .iter()
         .map(|line| {
             let mut tokens = line.split("bags contain");
-            let bag_type = tokens.next().unwrap().trim().to_string();
-            let children: Vec<(usize, String)> = tokens
+            let bag_type = tokens.next().unwrap().trim();
+            let children = tokens
                 .next()
                 .unwrap()
                 .split(',')
-                .map(|span| {
-                    let mut spans = span.split(' ');
-                    spans.next(); // skip blank
-                    let count = spans.next().unwrap().trim().parse::<usize>().ok();
-                    if let Some(num) = count {
-                        let bag_type: String = spans
-                            .take(2)
-                            .map(|word| {
-                                let mut w = word.to_string();
-                                w.push(' ');
-                                w
-                            })
-                            .collect();
-                        let bg = bag_type.trim().to_string();
-                        (num, bg)
-                    } else {
-                        (0, "".to_string())
-                    }
+                .map(|s| s.trim())
+                .filter_map(|span| {
+                    let mut spans = span.splitn(2, ' ');
+                    spans
+                        .next()
+                        .unwrap()
+                        .trim()
+                        .parse::<usize>()
+                        .ok()
+                        .map(|num| {
+                            (
+                                num,
+                                spans
+                                    .next()
+                                    .unwrap()
+                                    .trim_end_matches('.')
+                                    .trim_end_matches('s')
+                                    .trim_end_matches(&" bag"),
+                            )
+                        })
                 })
-                .collect();
-            // println!("{}: {:?}\n", bag_type, children);
-
+                .collect::<Vec<(usize, &str)>>();
             (bag_type, children)
         })
         .collect();
-    println!("start");
-    let bags = reee(&rules, "shiny gold");
-    Ok(bags.to_string())
-}
 
-fn reee(rules: &HashMap<String, Vec<(usize, String)>>, color: &str) -> usize {
     let mut bags = 0;
-    let mut unvisited_bags: Vec<(usize, String)> = vec![];
-    unvisited_bags.push((1, color.to_string()));
+    let mut unvisited_bags: Vec<(usize, &str)> = vec![];
+    unvisited_bags.push((1, "shiny gold"));
     while unvisited_bags.len() > 0 {
         let (next_count, next_bag) = unvisited_bags.pop().unwrap();
         if next_count > 0 {
-            println!("visiting {} \"{}\" bags", next_count, next_bag);
             let children = rules.get(&next_bag).unwrap();
-            for child in children.iter() {
-                let (cn, cc) = child;
-                println!("  inner bag: {} \"{}\" bags", cn, cc);
-                if *cn != 0 {
-                    let new = reee(&rules, &cc);
-                    let bo = bags;
-                    bags += next_count * (cn + (cn * new));
-                    println!("{} + ({}*({}*{})) = {}", bo, next_count, cn, new, bags);
-                    // bags += next_count + cn + (next_count * cn * new);
-                    // println!("{}", bags);
-                } else {
-                    println!("empty bag: {}", next_bag);
-                    // bags += 1;
+            for (child_num, child_color) in children.iter() {
+                if *child_num != 0 {
+                    let meta = next_count * child_num;
+                    unvisited_bags.push((meta, child_color));
+                    bags += meta;
                 }
             }
         }
     }
-    println!("found {} bags in the \"{}\" bag", bags, color);
-    bags
+    Ok(bags.to_string())
 }
-
-/*
-55966672 - too high
-270893567 -- higher so no
-265373032 -- still higher so no
-
-*/
