9ff8615f92708310e7bec8980c22cd643fc0f657
Daniel Boyle <boylede+github@gmail.com>
2020-12-17 21:33:37 -0800

    further day 13

--- src/day13.rs
+++ src/day13.rs
@@ -20,7 +20,7 @@ pub fn part1(lines: &Vec<String>) -> PartResult {
 }
 
 pub fn part2(lines: &Vec<String>) -> PartResult {
-    let mut rules = lines[1]
+    let rules = lines[1]
         .split(',')
         .enumerate()
         .filter_map(|(i, s)| {
@@ -31,70 +31,47 @@ pub fn part2(lines: &Vec<String>) -> PartResult {
                 None
             }
         })
-        // .inspect(|(i, b)| println!("bus {} at index {}", b, i))
-        // .map(|(i, b)| (i as i64, b))
-        // .max_by(|(i,b),(ii,bb)| b.cmp(bb))
-        // .unwrap();
         .collect::<Vec<(i64, i64)>>();
-    // sort the rules largest multiplier -> smallest
-    // rules.sort_by(|(_, b),(_, a)|a.cmp(b));
-    let (max_i, max) = rules.iter().max_by(|(i,b),(ii,bb)| b.cmp(bb)).unwrap();
-    println!("rules: {:?}", rules);
-    let slope: i64 = rules.iter().map(|(_,b)|b).product();
-    // let max_tries = u64::MAX / slope as u64;
-    // println!("number of tries: {}", i64::MAX / slope);
-    println!("determined slope: {}", slope);
-    let zeroth = rules.iter().filter(|(i, b)|*i==0).map(|(i,b)|b).next().unwrap();
 
+    let (max_i, max) = rules.iter().max_by(|(i, b), (ii, bb)| b.cmp(bb)).unwrap();
+    println!("rules: {:?}", rules);
+    let slope: i64 = rules.iter().map(|(_, b)| b).product();
+
+    println!("determined slope: {}", slope);
+    let zeroth = rules
+        .iter()
+        .filter(|(i, _)| *i == 0)
+        .map(|(_, b)| b)
+        .next()
+        .unwrap();
     println!("zeroth: {}", zeroth);
-    // for (offset, bus) in rules.iter() {
-    let rise;// = 0;
-    let mut n = 0;
+    // let rise;
+    let mut successes = vec![];
+    let mut n = 1;
+    let mut count = 0;
     loop {
         n += 1;
+        
         let test_number = (max * n) + max_i;
-        let correct = rules.iter().all(|(offset, bus) |{
-            (test_number - offset) % bus == 0
-            // test_number == (multiplier * bus) + offset
-        });
+        let correct = rules
+            .iter()
+            .all(|(offset, bus)| test_number % bus == *offset);
+        // print!(" {} ", test_number);
         if correct {
-            rise = test_number;
+            println!("{{{},{}}},", count, test_number);
+            successes.push(test_number);
+            // rise = test_number;
+            // break;
+            count +=1;
+        }
+        if count > 10 {
+            println!("");
             break;
         }
     }
+    let rise = successes.iter().next().unwrap();
     println!("slope: {}, rise: {}, result: {}", slope, rise, slope - rise);
-    panic!("");
-    // }
-    let mut i: i64 = -slope;
-    let mut count = 0;
-    let mut found = false;
-    while !found {
-        i += zeroth;
-        count += 1;
-        let test_num: i64 = slope + i;
-        // print!("{}: trying {} = {}x + {}", count, test_num, slope, i);
-        found = rules.iter().all(|(offset, b)| {
-            if (test_num + offset) % b == 0 {
-                // print!("|{}|", b);
-                true
-            } else {
-                false
-            }
-            
-        });
-        // print!("\n");
-        if count > slope {
-            panic!("");
-        }
-    }
-    println!("result found: y = {}x + {}", slope, i);
-    let result = 
-    // if i > 0 {
-        slope + i;
-    // } else {
-    //     -i
-    // };
-    // println!("y = {}", slope - i);
 
-    Ok(result.to_string())
+
+    Ok((slope - rise).to_string())
 }
